29d3a50576fdfc2be9b845700d6bd8c3d82cdabd
FiTz <filipmladenovik123@gmail.com>
2026-09-19 14:00:41 +0200

    stoicism source removed

--- Stoicism/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import './styles/base.css';
-import './styles/sections.css';
-
-if ('scrollRestoration' in history) history.scrollRestoration = 'manual';
-window.scrollTo(0, 0);
-
-createRoot(document.getElementById('root')).render(<App />);
